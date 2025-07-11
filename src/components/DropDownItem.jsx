b6c0e3ba471313c9f74b88a25c87f4b4ef49ef01
gandrica <gandrica1983@gmail.com>
2025-07-11 16:25:46 +0200

    Mise à jour logique composant DropDown et SASS LocationPage et ses enfants

--- src/components/DropDownItem.jsx
+++ src/components/DropDownItem.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import styles from "../../public/scss/components/DropDownItem.module.scss";
 function DropDownItem({ item }) {
   const { title, texte } = item;
+  const [visible, setVisible] = useState(false);
   return (
-    <div>
-      <h4>{title}</h4>
-      <p>{texte}</p>
+    <div className={styles.dropDownItem}>
+      <h4
+        onClick={() => {
+          console.log(visible);
+          setVisible(!visible);
+        }}
+      >
+        {title}
+        {visible ? (
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
+          </span>
+        ) : (
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" />
+            </svg>
+          </span>
+        )}
+      </h4>
+      {visible && <p>{texte}</p>}
     </div>
   );
 }
