a9133b2ef313d00c5ba989632e1f869b9e9ac6dc
gandrica <gandrica1983@gmail.com>
2025-09-17 16:17:28 +0200

    Mise à jour du scss du site - suite 5 - Corrections Responsive et PageIntrouvable

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -2,7 +2,7 @@ import styles from "../../public/scss/components/ErrorComponent.module.scss";
 import { Link } from "react-router-dom";
 function ErrorComponent() {
   return (
-    <div className={styles.errorPage}>
+    <div className={styles.error}>
       <h2>404</h2>
       <p>Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/">Retourner sur la page d'accueil</Link>
